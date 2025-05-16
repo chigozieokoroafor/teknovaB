c12b98317b7301554d8e28d3bad689ce14cee8bf
okoroaforchigozie <okoroaforc14@gmail.com>
2025-05-16 15:42:49 -0200

    update : tryign to fix deploymen

--- src/db/sync.js
+++ src/db/sync.js
@@ -16,16 +16,16 @@ exports.sync = async () => {
     conn.authenticate().then(async () => {
         await Promise.allSettled(
             [
-                // user.sync({ alter: true }),
-                // admin.sync({ alter: true }),
-                // cart.sync({ alter: true }),
-                // category.sync({ alter: true }),
-                // images.sync({alter:true}),
-                // order.sync({alter:true}),
-                // product.sync({alter:true}),
-                // review.sync({alter:true}),
-                // shipping.sync({alter:true}),
-                // transaction.sync({alter:true})
+                user.sync({ alter: true }),
+                admin.sync({ alter: true }),
+                cart.sync({ alter: true }),
+                category.sync({ alter: true }),
+                images.sync({alter:true}),
+                order.sync({alter:true}),
+                product.sync({alter:true}),
+                review.sync({alter:true}),
+                shipping.sync({alter:true}),
+                transaction.sync({alter:true})
 
             ]
         )
