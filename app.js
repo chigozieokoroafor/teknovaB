fe90b42477a7bac8164b894f499e4a739835130a
chigozie <okoroaforc14@gmail.com>
2026-04-26 21:02:52 +0100

    chore: added cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const morgan = require("morgan")
 
 const app = express()
 
-app.use(cors({ origin: "*" }))
+app.use(cors({ origin: "http://69.169.110.131:5173" }))
 app.use(morgan("dev"));
 
 app.use(express.json({ limit: '50mb' }));
@@ -27,7 +27,6 @@ app.use("/", (req, res) => {
     return notFound(res, `Path not found : ${req.url}`)
     // (res, {}, `Welcome to TekNova. Kindly note path specified not found {} `)
 })
-
 
 app.use(errorHandler)
 
